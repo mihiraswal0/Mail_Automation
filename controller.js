018019ab375a424dd75e107e21248f59ee75d626
Mihir Aswal <montyaswal0@gmail.com>
2023-11-28 03:57:45 +0530

    check for unread mails to whom not responed done

--- controller.js
+++ controller.js
@@ -1,29 +1,100 @@
-const axios=require('axios');
-const {OAuth2Client}=require('google-auth-library');
-const { oauth2 } = require('googleapis/build/src/apis/oauth2');
-require('dotenv').config();
+const axios = require("axios");
+const { OAuth2Client } = require("google-auth-library");
+const { oauth2 } = require("googleapis/build/src/apis/oauth2");
+require("dotenv").config();
 
 const oAuth2Client = new OAuth2Client(
-    process.env.CLIENT_ID,process.env.CLIENT_SECRET,process.env.REDIRECT_URL
+  process.env.CLIENT_ID,
+  process.env.CLIENT_SECRET,
+  process.env.REDIRECT_URL
 );
 
-const mailReply=async(req,res)=>{
-    console.log(req.session.tokens)
-    oAuth2Client.setCredentials({refresh_token:req.session.tokens.refresh_token});
-    const {token} = await oAuth2Client.getAccessToken();
-    // res.send("mail");
-    try{
-    const response = await axios.get('https://www.googleapis.com/gmail/v1/users/me/threads?q=is:unread is:inbox category:primary', {
-        method: 'GET',
-        headers:{
-            'Authorization': `Bearer ${token}`
-        }
+const mailReply = async (req, res) => {
+  oAuth2Client.setCredentials({
+    refresh_token: req.session.tokens.refresh_token,
   });
-  res.send(response.data);
-}
-catch(err)
-{
+  const { token } = await oAuth2Client.getAccessToken();
+  // res.send("mail");
+  try {
+    const unReadMails=await getUnreadMail(token);
+    // console.log(unReadMails)
+    unReadMails.threads.map(async(mail)=>{
+        const messagesInThread=await getMailDetails(mail.id,token);
+        console.log(messagesInThread)
+        let isReplied=false;
+        for(i in messagesInThread)
+        {
+           const messageHeader=await getMessageHeader(messagesInThread[i].id,token);
+           if(messageHeader.length && messageHeader[0].value.includes(req.session.user))
+           {
+            isReplied=true;
+            break;
+           }
+        }
+       
+    })
+    res.send("done");
+
+    
+  } catch (err) {
+
     res.send(err);
+  }
+};
+const getUnreadMail=async(token)=>{
+    try{
+    const response = await axios.get(
+        "https://www.googleapis.com/gmail/v1/users/me/threads?q=is:unread is:inbox category:primary &maxResults=1",
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+         
+        }
+      );
+      
+      return response.data;
+    }
+      catch (err) {
+        throw err;
+      }
 }
+const getMailDetails=async(id,token)=>{
+    try{
+        const response=await axios.get(` https://gmail.googleapis.com/gmail/v1/users/me/threads/${id}`,{
+            headers:{
+                Authorization: `Bearer ${token}`,
+            },
+            params: {
+                format: 'minimal',
+              },
+        }
+        )
+        return response.data.messages;
+    }
+    catch(err){
+        throw err;
+    }
+ 
+
 }
-module.exports ={mailReply}
+const getMessageHeader=async(id,token)=>{
+    try{
+    const response=await axios.get(`https://gmail.googleapis.com/gmail/v1/users/me/messages/${id}`,{
+        headers: {
+            Authorization: `Bearer ${token}`,
+        },
+        params: {
+            format: 'metadata',
+            metadataHeaders:'From'
+          },
+    }
+    );
+    return response.data.payload.headers;
+}
+    catch(err){
+        throw err;
+    }
+
+}
+module.exports = { mailReply };
--- loginRoutes/controller.js
+++ loginRoutes/controller.js
@@ -1,11 +1,12 @@
 const {OAuth2Client}=require('google-auth-library');
+const axios = require('axios');
 require('dotenv').config();
 const oAuth2Client = new OAuth2Client(process.env.CLIENT_ID, process.env.CLIENT_SECRET, process.env.REDIRECT_URL);
 
 const googleLogin=(req,res)=>{
     const authUrl = oAuth2Client.generateAuthUrl({
         access_type: 'offline',
-        scope: ['https://www.googleapis.com/auth/gmail.compose','https://www.googleapis.com/auth/gmail.readonly']
+        scope: ['https://www.googleapis.com/auth/gmail.readonly','https://www.googleapis.com/auth/userinfo.profile']
       });
       res.redirect(authUrl);
 }
@@ -16,9 +17,9 @@ const googleCallback=async(req,res)=>{
       const { tokens } = await oAuth2Client.getToken(code);  
     //   console.log(tokens);
       req.session.tokens=tokens;
+      req.session.user=await getUserDetails(tokens.access_token);
       res.send('Login successful! .');
     } catch (error) {
-      console.error(error);
       res.status(500).send('Login failed. Please try again.');
     }
 }
@@ -28,5 +29,18 @@ const googleLogout=(req,res)=>{
         res.redirect('/');
       });
 }
+const getUserDetails=async(token)=>{
+  try {
+    const response = await axios.get('https://gmail.googleapis.com/gmail/v1/users/me/profile', {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
 
+
+    return response.data.emailAddress;
+  } catch (error) {
+    throw(error);
+  }
+}
 module.exports={googleCallback,googleLogout,googleLogin};
